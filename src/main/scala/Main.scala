bb1f67f258c38f9d6173b23c3336383212645d55
firephil <philip@firesoft.gr>
2021-04-30 03:55:39 +0300

    code cleanup

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,7 @@
 package mandelbrotset
-import javax.swing.ImageIcon
 import javax.swing.JFrame
-import javax.swing.JLabel
 import java.awt.BorderLayout
 import java.awt.image.BufferedImage
-import java.awt.Dimension
 import java.awt.Toolkit
 import javax.swing.WindowConstants
 
