cbfeea0860fe05b10b53559eb3a949da64e4373f
firephil <philip@firesoft.gr>
2021-05-02 23:50:33 +0300

    Update Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -10,7 +10,8 @@ def run() =
     val frame = new JFrame("Mandelbrot Set")  
     var size = frame.getToolkit().getScreenSize() // get the dimensions of the computer screen
     
-    val imgMandel=Mandelbrot.generate(size.width-10,size.height-10)
+    //val imgMandel=Mandelbrot.generate(size.width-10,size.height-10)
+    val imgMandel=Mandelbrot.generate(4000,3000)
     val zoomPanel = new ZoomPanel(imgMandel.image)
     
     frame.getContentPane.add(zoomPanel, BorderLayout.CENTER)
